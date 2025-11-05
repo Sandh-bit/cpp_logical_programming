8bd43294ddfc257a4b10a87470899d7b0afaa47b
Sandh-bit <sandhyacj889622@gmail.com>
2025-11-05 22:15:53 +0530

    Create remove_duplicates_in_a_string.cpp

---
+++ remove_duplicates_in_a_string.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+char* removeduplicates(char str[],int n)
+{
+  int index=0;
+  for(int i=0;i<n;i++)
+  {
+    int j;
+    for(j=0;j<i;j++)
+    {
+      if(str[i]==str[j])
+      
+        break;
+    }
+      if(j==i)
+      
+        str[index++]=str[i];
+      
+    
+  }
+  str[index]='\0';
+  return str;
+}
+int main()
+{
+  char str[]="AMMAAPPA";
+  int n=sizeof(str)/sizeof(str[0]);
+  cout<<removeduplicates(str,n)<<endl;
+  cout<<str<<endl;
+  return 0;
+  
+}
